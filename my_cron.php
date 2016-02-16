c718016e41225ec7fc1f76f4958512624751a46d
regvo <admin@regvo.ru>
2016-02-16 10:05:24 +0300

    Delete my_cron.php

--- my_cron.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  if(PHP_SAPI != 'cli') { die('Access denied'); }
-  $fd = fopen("/home/snn/www/ins.dev/my_cron.txt","a"); 
-  fwrite($fd, "Обращение к файлу - ".date("d.m.Y H:i")."\r\n"); 
-  fclose($fd); 
-?>
